5117e4b5af5c1095d620d2526d8de4ddf54997f5
Michael <mikecook450@gmail.com>
2024-08-09 12:35:37 -0400

    03.  Projects

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
+import Projects from "./components/Projects";
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <div className="relative z-10 flex flex-col items-center p-4 space-y-8 container mx-auto">
         <Hero />
         <Navbar />
+        <Projects />
       </div>
     </div>
   );
