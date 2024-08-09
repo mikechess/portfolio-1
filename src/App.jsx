35a11776dc8fe77533df9574d348308c158a361c
Michael <mikecook450@gmail.com>
2024-08-09 12:14:53 -0400

    02. Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import Hero from "./components/Hero";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
@@ -6,6 +7,7 @@ function App() {
       <div className="fixed inset-0 bg-fixed bg-cover bg-center bg-img"></div>
       <div className="relative z-10 flex flex-col items-center p-4 space-y-8 container mx-auto">
         <Hero />
+        <Navbar />
       </div>
     </div>
   );
