a516dcfcc01017a48305ed8d6bde705864f3ce07
Michael <mikecook450@gmail.com>
2024-08-09 16:32:43 -0400

    06. Work Experience

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
 import Projects from "./components/Projects";
 import Skills from "./components/Skills";
+import WorkExperience from "./components/WorkExperience";
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
         <Projects />
         <Bio />
         <Skills />
+        <WorkExperience />
       </div>
     </div>
   );
